d719eeed9c56e7bca8c81e9234379b296dc5e40f
yuto.moriizumi <kurvan1112@gmail.com>
2024-10-23 17:44:24 +0900

    Fixed lockfile and dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,28 @@ module.exports = (env, argv) => {
   return {
     mode: "production",
     entry: {
-      index: path.join(__dirname, "src", "index.ts")
+      index: path.join(__dirname, "src", "index.ts"),
     },
     output: {
       path: path.join(__dirname, "www"),
       filename: "PixiGame.js",
       library: "PixiGame",
-      libraryTarget: "umd"
+      libraryTarget: "umd",
     },
     module: {
       rules: [
         {
           test: /\.ts$/,
-          use: [{ loader: "ts-loader" }]
-        }
-      ]
+          use: [{ loader: "ts-loader" }],
+        },
+      ],
     },
     resolve: {
       extensions: [".ts", ".js"],
-      modules: ["node_modules"]
-    }
+      modules: ["node_modules"],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "www"),
+    },
   };
 };
